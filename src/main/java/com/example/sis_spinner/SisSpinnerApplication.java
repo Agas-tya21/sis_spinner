5b30ffb33773a15f500e0be7409db94e425b253b
agas <agasp435@gmail.com>
2025-12-02 09:51:30 +0700

    first commit

---
+++ src/main/java/com/example/sis_spinner/SisSpinnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sis_spinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisSpinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisSpinnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sis_spinner/SisSpinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sis_spinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisSpinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
